ca195801e7cfaa7e8576ebfb1bd19115c31de031
Mihai Pavelescu <media.expres@gmail.com>
2022-06-04 17:03:01 +0300

    added addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,29 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items' />
-            <Button>PLĂTEȘTE</Button>
-        </div>
-    );
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>COSUL ESTE GOL</span>
+        )}
+      </div>
+      <Button>PLATESTE</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,18 +1,26 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+    
 import './product-card.styles.scss';
 
 import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (<div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`} />
         <div className='footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
-            <span>lei</span>    
+            <span>&#160;lei</span>    
             </div>
-        <Button buttonType='inverted'>În coș</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>
+            În coș
+        </Button>
         </div>
         );
     }
