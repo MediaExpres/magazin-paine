a565aad63231cba1a0d700cf6e646e62c551c152
Mihai Pavelescu <media.expres@gmail.com>
2022-11-22 14:24:03 +0200

    update things as of 22.11.2022

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
 import { UserProvider } from './contexts/user.context.jsx';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+  
 import './index.scss';
 
 const rootElement = document.getElementById('root');
@@ -17,7 +19,9 @@ render(
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-            <App />
+            <Elements stripe={stripePromise}>
+              <App />
+            </Elements>
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>  
